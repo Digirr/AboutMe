f17d0589fc837a99157e6a56b97b571bad84ac64
Kamil Tyszkiewicz <kamil.tyszkiewicz47@gmail.com>
2021-04-15 18:48:49 +0200

    Finally done app

--- app/src/main/java/com/digir/aboutme/MainActivity.kt
+++ app/src/main/java/com/digir/aboutme/MainActivity.kt
@@ -8,31 +8,48 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.digir.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var B : ActivityMainBinding
 
+    //Constructor use only first value, not both
+    private val myName: MyName = MyName("Kamil Tyszkiewicz")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        B = ActivityMainBinding.bind(findViewById(R.id.root))
+        //setContentView(R.layout.activity_main)
+        B = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        B.myName = myName   //set with databinding
+
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            //'it' is reference to the done button which is view passed as an argument
+//            addNickname(it)
+//        }
+        B.doneButton.setOnClickListener {
             //'it' is reference to the done button which is view passed as an argument
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        B.apply {
+            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll() //We must invalidate all binding expressions which get created with correct data
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
 
         //Hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ app/src/main/java/com/digir/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.digir.aboutme
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
