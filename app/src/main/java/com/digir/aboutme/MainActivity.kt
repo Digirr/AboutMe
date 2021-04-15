c313fbf3e2bb2b5aadebc636d0e6975f40eb9dff
Kamil Tyszkiewicz <kamil.tyszkiewicz47@gmail.com>
2021-04-15 17:37:59 +0200

    Added nickname EditText

--- app/src/main/java/com/digir/aboutme/MainActivity.kt
+++ app/src/main/java/com/digir/aboutme/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.digir.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.digir.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var B : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        B = ActivityMainBinding.bind(findViewById(R.id.root))
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            //'it' is reference to the done button which is view passed as an argument
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
